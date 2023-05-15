77a543eb25cdf71ec1f0531244c696697594ac33
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2023-05-15 17:09:27 -0400

    Added a TODO for webhooks

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -5,5 +5,7 @@ import { app } from "../shopify/app.server";
 export const action = async ({ request }: ActionArgs) => {
   const { admin } = await app.authenticate.webhook(request);
 
+  // TODO: Add a switch statement on the topic catching the mandatory GDPR topics
+
   return new Response(null, { status: 200 });
 };
