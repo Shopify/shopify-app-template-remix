e2909ead30d02da52f1614c6eb16caf743f693b1
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2023-05-16 13:48:24 -0400

    Add TODO about CLI env vars breaking remix app

--- remix.config.js
+++ remix.config.js
@@ -1,3 +1,5 @@
+// TODO: Document this properly somewhere. Where? We should discuss this issue with the CLI team.
+// Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
 // stop passing in HOST, so we can remove this workaround after the next major release.
 if (process.env.HOST) {
