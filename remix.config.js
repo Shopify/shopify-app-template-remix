6ebebfdcb5451076bf08498fe27d874a738e40c2
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2023-05-16 15:29:06 -0400

    Update workaround for HOST env var

--- remix.config.js
+++ remix.config.js
@@ -2,7 +2,11 @@
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
 // stop passing in HOST, so we can remove this workaround after the next major release.
-if (process.env.HOST) {
+if (
+  process.env.HOST &&
+  (!process.env.SHOPIFY_APP_URL ||
+    process.env.SHOPIFY_APP_URL === process.env.HOST)
+) {
   const url = new URL(process.env.HOST);
   if (url.hostname === "localhost") {
     url.port = process.env.PORT;
