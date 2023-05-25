553aa540662c45ba554d4bc14f43604c96ec252d
Richard Powell <richard.powell@shopify.com>
2023-05-25 16:10:40 -0400

    Add tests to check we respondcorrectly when it's the exitIFrame route

---
+++ shopify-app-remix/src/auth/admin/__tests__/exitIFramePath.test.ts
@@ -0,0 +1,59 @@
+import { shopifyApp } from "../../..";
+import { getThrownResponse, testConfig } from "../../../__tests__/test-helper";
+
+describe("authorize.admin", () => {
+  test("Uses App Bridge to exit iFrame when the url matches auth.exitIframePath", async () => {
+    // GIVEN
+    const config = testConfig();
+    const shopify = shopifyApp(testConfig());
+
+    // WHEN
+    const exitTo = encodeURIComponent(config.appUrl);
+    const url = `${shopify.config.appUrl}/auth/exit-iframe?exitIframe=${exitTo}`;
+    const response = await getThrownResponse(
+      shopify.authenticate.admin,
+      new Request(url)
+    );
+
+    // THEN
+    const responseText = await response.text();
+    expect(response.status).toBe(200);
+    expect(response.headers.get("content-type")).toBe(
+      "text/html;charset=utf-8"
+    );
+    expect(responseText).toContain(
+      `<script data-api-key="${config.apiKey}" src="https://cdn.shopify.com/shopifycloud/app-bridge-next/app-bridge.js"></script>`
+    );
+    expect(responseText).toContain(
+      `<script>shopify.redirectTo("${decodeURIComponent(exitTo)}")</script>`
+    );
+  });
+
+  test("Uses App Bridge to exit iFrame when the url matches auth.exitIframePath and authPathPrefix is passed", async () => {
+    // GIVEN
+    const authPathPrefix = "/shopify";
+    const config = testConfig({ authPathPrefix });
+    const shopify = shopifyApp(config);
+
+    // WHEN
+    const exitTo = encodeURIComponent(config.appUrl);
+    const url = `${shopify.config.appUrl}${authPathPrefix}/exit-iframe?exitIframe=${exitTo}`;
+    const response = await getThrownResponse(
+      shopify.authenticate.admin,
+      new Request(url)
+    );
+
+    // THEN
+    const responseText = await response.text();
+    expect(response.status).toBe(200);
+    expect(response.headers.get("content-type")).toBe(
+      "text/html;charset=utf-8"
+    );
+    expect(responseText).toContain(
+      `<script data-api-key="${config.apiKey}" src="https://cdn.shopify.com/shopifycloud/app-bridge-next/app-bridge.js"></script>`
+    );
+    expect(responseText).toContain(
+      `<script>shopify.redirectTo("${decodeURIComponent(exitTo)}")</script>`
+    );
+  });
+});
