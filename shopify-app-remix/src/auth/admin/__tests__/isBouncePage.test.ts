d9d0ea349d294659a5cafc88cd8057589998aa99
Richard Powell <richard.powell@shopify.com>
2023-05-25 14:00:24 -0400

    Add tests for when the URL is the bounce page URL

---
+++ shopify-app-remix/src/auth/admin/__tests__/isBouncePage.test.ts
@@ -0,0 +1,49 @@
+import { shopifyApp } from "../../..";
+import { getThrownResponse, testConfig } from "../../../__tests__/test-helper";
+
+describe("authorize.admin", () => {
+  test("Uses AppBridge to get a session token if the URL is for session-token page", async () => {
+    // GIVEN
+    const config = testConfig();
+    const shopify = shopifyApp(config);
+
+    // WHEN
+    const url = `${shopify.config.appUrl}/auth/session-token`;
+    const response = await getThrownResponse(
+      shopify.authenticate.admin,
+      new Request(url)
+    );
+
+    // THEN
+    expect(response.status).toBe(200);
+    expect(response.headers.get("content-type")).toBe(
+      "text/html;charset=utf-8"
+    );
+    expect((await response.text()).trim()).toBe(
+      `<script data-api-key="${config.apiKey}" src="https://cdn.shopify.com/shopifycloud/app-bridge-next/app-bridge.js"></script>`
+    );
+  });
+
+  test("Uses AppBridge to get a session token if the URL is for configured session-token page", async () => {
+    // GIVEN
+    const authPathPrefix = "/shopify";
+    const config = testConfig({ authPathPrefix });
+    const shopify = shopifyApp(config);
+
+    // WHEN
+    const url = `${shopify.config.appUrl}${authPathPrefix}/session-token`;
+    const response = await getThrownResponse(
+      shopify.authenticate.admin,
+      new Request(url)
+    );
+
+    // THEN
+    expect(response.status).toBe(200);
+    expect(response.headers.get("content-type")).toBe(
+      "text/html;charset=utf-8"
+    );
+    expect((await response.text()).trim()).toContain(
+      `<script data-api-key="${config.apiKey}" src="https://cdn.shopify.com/shopifycloud/app-bridge-next/app-bridge.js"></script>`
+    );
+  });
+});
