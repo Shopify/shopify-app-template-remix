0eb6c5216fef6380574b4a7985cbeed48a09ef13
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2023-05-30 13:18:41 -0400

    Adding missing tests

--- shopify-app-remix/src/auth/admin/__tests__/session-token-header-path.test.ts
+++ shopify-app-remix/src/auth/admin/__tests__/session-token-header-path.test.ts
@@ -1,3 +1,5 @@
+import { SESSION_COOKIE_NAME, Session } from "@shopify/shopify-api";
+
 import { shopifyApp } from "../../..";
 import {
   BASE64_HOST,
@@ -5,6 +7,7 @@ import {
   getJwt,
   getThrownResponse,
   setUpValidSession,
+  signRequestCookie,
   testConfig,
 } from "../../../__tests__/test-helper";
 
@@ -47,18 +50,51 @@ describe("authorize.session token header path", () => {
 
         // THEN
         expect(response.status).toBe(401);
-        expect(
-          response.headers.get("X-Shopify-API-Request-Failure-Reauthorize-Url")
-        ).toBe(
-          `${shopify.config.appUrl}${shopify.config.auth.path}?shop=${TEST_SHOP}`
+
+        const { origin, pathname, searchParams } = new URL(
+          response.headers.get("X-Shopify-API-Request-Failure-Reauthorize-Url")!
         );
+        expect(origin).toBe(shopify.config.appUrl);
+        expect(pathname).toBe(shopify.config.auth.path);
+        expect(searchParams.get("shop")).toBe(TEST_SHOP);
+      });
+
+      it(`returns app bridge redirection headers if the session is no longer valid (isOnline: ${isOnline})`, async () => {
+        // GIVEN
+        const shopify = shopifyApp(
+          testConfig({ useOnlineTokens: isOnline, scopes: ["otherTestScope"] })
+        );
+        await setUpValidSession(shopify.config.sessionStorage, isOnline);
+
+        // WHEN
+        const { token } = getJwt(
+          shopify.config.apiKey,
+          shopify.config.apiSecretKey
+        );
+        const response = await getThrownResponse(
+          shopify.authenticate.admin,
+          new Request(
+            `${shopify.config.appUrl}?shop=${TEST_SHOP}&host=${BASE64_HOST}`,
+            { headers: { Authorization: `Bearer ${token}` } }
+          )
+        );
+
+        // THEN
+        expect(response.status).toBe(401);
+
+        const { origin, pathname, searchParams } = new URL(
+          response.headers.get("X-Shopify-API-Request-Failure-Reauthorize-Url")!
+        );
+        expect(origin).toBe(shopify.config.appUrl);
+        expect(pathname).toBe(shopify.config.auth.path);
+        expect(searchParams.get("shop")).toBe(TEST_SHOP);
       });
     });
   });
 
   describe("success", () => {
     [true, false].forEach((isOnline) => {
-      it(`returns context when session exists (isOnline: ${isOnline})`, async () => {
+      it(`returns context when session exists for embedded apps (isOnline: ${isOnline})`, async () => {
         // GIVEN
         const shopify = shopifyApp(testConfig({ useOnlineTokens: isOnline }));
 
@@ -82,6 +118,41 @@ describe("authorize.session token header path", () => {
 
         // THEN
         expect(sessionToken).toEqual(payload);
+        expect(session).toBe(testSession);
+        expect(admin.rest.session).toBe(testSession);
+        expect(admin.graphql.session).toBe(testSession);
+      });
+
+      it(`returns context when session exists for non-embedded apps (isOnline: ${isOnline})`, async () => {
+        // GIVEN
+        const shopify = shopifyApp({
+          ...testConfig(),
+          isEmbeddedApp: false,
+          useOnlineTokens: isOnline,
+        });
+
+        let testSession: Session;
+        testSession = await setUpValidSession(shopify.config.sessionStorage);
+        if (isOnline) {
+          testSession = await setUpValidSession(
+            shopify.config.sessionStorage,
+            true
+          );
+        }
+
+        // WHEN
+        const request = new Request(
+          `${shopify.config.appUrl}?shop=${TEST_SHOP}&host=${BASE64_HOST}`
+        );
+        signRequestCookie({
+          request,
+          apiSecretKey: shopify.config.apiSecretKey,
+          cookieName: SESSION_COOKIE_NAME,
+          cookieValue: testSession.id,
+        });
+        const { admin, session } = await shopify.authenticate.admin(request);
+
+        // THEN
         expect(session).toBe(testSession);
         expect(admin.rest.session).toBe(testSession);
         expect(admin.graphql.session).toBe(testSession);
