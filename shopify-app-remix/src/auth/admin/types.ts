5dee27de4a2b2c0d5fe44b3a22be27510817aff0
Richard Powell <richard.powell@shopify.com>
2023-06-02 15:28:15 -0400

    Add TSDOC for properties on the top level of the admin context

--- shopify-app-remix/src/auth/admin/types.ts
+++ shopify-app-remix/src/auth/admin/types.ts
@@ -11,8 +11,59 @@ interface AdminContextInternal<
   Config extends AppConfigArg,
   Resources extends ShopifyRestResources = ShopifyRestResources
 > {
+  /**
+   * The session for the user who made the request.
+   *
+   * Use this to get shop or user specific data.
+   *
+   * @example
+   * Getting your app's shop specific widget data using an offline session
+   * ```ts
+   * // app/routes/**\/.ts
+   * import { LoaderArgs, json } from "@remix-run/node";
+   * import { shopify } from "../shopify.server";
+   * import { getWidgets } from "~/db/widgets.server";
+   *
+   * export const loader = async ({ request }: LoaderArgs) => {
+   *   const { session } = await shopify.authenticate.admin(
+   *     request
+   *   );
+   *   return json(await getWidgets({shop: session.shop));
+   * };
+   * ```
+   *
+   * @example
+   * Getting your app's user specific widget data using an online session
+   * ```ts
+   * // shopify.server.ts
+   * import { shopifyApp } from "@shopify/shopify-app-remix";
+   *
+   * export const shopify = shopifyApp({
+   *   // ...etc
+   *   useOnlineTokens: true,
+   * });
+   *
+   * // app/routes/**\/.ts
+   * import { LoaderArgs, json } from "@remix-run/node";
+   * import { shopify } from "../shopify.server";
+   * import { getWidgets } from "~/db/widgets.server";
+   *
+   * export const loader = async ({ request }: LoaderArgs) => {
+   *   const { session } = await shopify.authenticate.admin(
+   *     request
+   *   );
+   *   return json(await getWidgets({user: session.onlineAccessInfo!.id}));
+   * };
+   * ```
+   */
   session: Session;
+  /**
+   * Methods for interacting with the Shopify REST Admin API for this store
+   */
   admin: AdminApiContext<Resources>;
+  /**
+   * Billing methods for this store
+   */
   billing: BillingContext<Config>;
 }
 
@@ -20,6 +71,35 @@ export interface EmbeddedAdminContext<
   Config extends AppConfigArg,
   Resources extends ShopifyRestResources = ShopifyRestResources
 > extends AdminContextInternal<Config, Resources> {
+  /**
+   * The decoded and validated session token for the request
+   *
+   * {@link https://shopify.dev/docs/apps/auth/oauth/session-tokens#payload}
+   *
+   * @example
+   * Getting your app's user specific widget data using the session token
+   * ```ts
+   * // shopify.server.ts
+   * import { shopifyApp } from "@shopify/shopify-app-remix";
+   *
+   * export const shopify = shopifyApp({
+   *   // ...etc
+   *   useOnlineTokens: true,
+   * });
+   *
+   * // app/routes/**\/.ts
+   * import { LoaderArgs, json } from "@remix-run/node";
+   * import { shopify } from "../shopify.server";
+   * import { getWidgets } from "~/db/widgets.server";
+   *
+   * export const loader = async ({ request }: LoaderArgs) => {
+   *   const { sessionToken } = await shopify.authenticate.storefront(
+   *     request
+   *   );
+   *   return json(await getWidgets({user: sessionToken.sub}));
+   * };
+   * ```
+   */
   sessionToken: JwtPayload;
 }
 export interface NonEmbeddedAdminContext<
