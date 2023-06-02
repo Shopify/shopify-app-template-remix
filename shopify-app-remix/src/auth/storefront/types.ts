90266d78698f7cdf98ce820ca8961faed67a415d
Richard Powell <richard.powell@shopify.com>
2023-06-02 14:48:43 -0400

    Add tsdocs for the StorefrontContext

--- shopify-app-remix/src/auth/storefront/types.ts
+++ shopify-app-remix/src/auth/storefront/types.ts
@@ -1,5 +1,27 @@
 import { JwtPayload } from "@shopify/shopify-api";
 
+/**
+ * Authenticated Context for a storefront request
+ */
 export interface StorefrontContext {
+  /**
+   * The decoded and validated session token for the request
+   *
+   * {@link https://shopify.dev/docs/apps/auth/oauth/session-tokens#payload}
+   *
+   * @example
+   * Getting your app's user specific widget data using the session token
+   * // app/routes/**\/.ts
+   * import { LoaderArgs, json } from "@remix-run/node";
+   * import { shopify } from "../shopify.server";
+   * import { getWidgets } from "~/db/widgets.server";
+   *
+   * export const loader = async ({ request }: LoaderArgs) => {
+   *   const { sessionToken } = await shopify.authenticate.storefront(
+   *     request
+   *   );
+   *   return json(await getWidgets({user: sessionToken.sub}));
+   * };
+   */
   sessionToken: JwtPayload;
 }
