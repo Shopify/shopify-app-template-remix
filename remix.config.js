d6b6f90fd8ad53ace4f51e07ab786659ae9cc97e
Richard Powell <richard.powell@shopify.com>
2023-06-05 15:45:00 -0400

    Use V2 Routes

--- remix.config.js
+++ remix.config.js
@@ -15,7 +15,7 @@ if (
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/"
+  future: {
+    v2_routeConvention: true,
+  },
 };
