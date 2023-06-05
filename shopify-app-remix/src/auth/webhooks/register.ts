14ea9955de29857ad871ade8fe4bdc0d6ab40ee1
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2023-06-05 10:04:25 -0400

    Applying comment from review

--- shopify-app-remix/src/auth/webhooks/register.ts
+++ shopify-app-remix/src/auth/webhooks/register.ts
@@ -6,18 +6,18 @@ export function registerWebhooksFactory({ api, logger }: BasicParams) {
   return async function registerWebhooks({ session }: RegisterWebhooksOptions) {
     return api.webhooks.register({ session }).then((response) => {
       Object.entries(response).forEach(([topic, topicResults]) => {
-        topicResults.forEach(({ success, ...result }) => {
+        topicResults.forEach(({ success, ...rest }) => {
           if (success) {
             logger.debug("Registered webhook", {
               topic,
               shop: session.shop,
-              operation: result.operation,
+              operation: rest.operation,
             });
           } else {
             logger.error("Failed to register webhook", {
               topic,
               shop: session.shop,
-              result: result.result,
+              result: rest.result,
             });
           }
         });
