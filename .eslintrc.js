bb91c02504d5fe1a0f04d642d4a35483edbb64b6
Richard Powell <richard.powell@shopify.com>
2023-06-07 10:11:42 -0400

    Add basic eslint config copied from the Remix Blues stack

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+/** @type {import('@types/eslint').Linter.BaseConfig} */
+module.exports = {
+  root: true,
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "@remix-run/eslint-config/jest-testing-library",
+    "prettier",
+  ],
+};
