f2d9d450b46e5c3fb297954aa41587fae374917b
Richard Powell <richard.powell@shopify.com>
2023-06-07 10:46:58 -0400

    Add some Shopify specific eslint and prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,17 @@
 module.exports = {
   root: true,
   extends: [
-    "@remix-run/eslint-config",
-    "@remix-run/eslint-config/node",
-    "@remix-run/eslint-config/jest-testing-library",
-    "prettier",
+    '@remix-run/eslint-config',
+    '@remix-run/eslint-config/node',
+    '@remix-run/eslint-config/jest-testing-library',
+    'plugin:@shopify/prettier',
+    'plugin:@shopify/typescript',
+    'plugin:@shopify/polaris',
   ],
+  rules: {
+    '@shopify/images-no-direct-imports': 'off',
+    '@shopify/binary-assignment-parens': 'off',
+    'no-process-env': 'off',
+    'import/extensions': 'off',
+  },
 };
