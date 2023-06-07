53ebda14a3819e026b249ac71b258324ca0417b2
Richard Powell <richard.powell@shopify.com>
2023-06-07 11:37:46 -0400

    Add .editorconfig

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -7,7 +7,7 @@ import {
 import {PrismaSessionStorage} from '@shopify/shopify-app-session-storage-prisma';
 import {restResources} from '@shopify/shopify-api/rest/admin/2023-04';
 
-import prisma from '~/db.server';
+import prisma from './db.server';
 
 export const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY!,
