408b9f0e825d9514d0201c8b50ed99035ed0c50c
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2023-06-09 11:44:01 -0400

    Created a QRCode model, and created a public API endpoint for scanning codes

---
+++ app/routes/qrcodes.$id.scan.tsx
@@ -0,0 +1,18 @@
+import type { LoaderArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import invariant from "tiny-invariant";
+
+import { getQRCode, incrementScanCount } from "../models/QRCode";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  invariant(params.id, "Could not find QR code destination");
+
+  const id = Number(params.id);
+  const qrCode = await getQRCode(id);
+
+  invariant(qrCode, "Could not find QR code destination");
+
+  await incrementScanCount(id);
+
+  return redirect(qrCode.destinationUrl);
+};
