3c443b692091ab81fc4d7050d316ea098aba089f
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2023-06-16 08:45:42 -0400

    Update shopify-app-remix/src/auth/admin/authenticate.ts
    Co-authored-by: Jason Miller <developit@users.noreply.github.com>

--- shopify-app-remix/src/auth/admin/authenticate.ts
+++ shopify-app-remix/src/auth/admin/authenticate.ts
@@ -499,7 +499,12 @@ export class AuthStrategy<
 
   private respondToOptionsRequest(request: Request) {
     if (request.method === "OPTIONS") {
-      throw new Response(null, { headers: APP_BRIDGE_HEADERS });
+      throw new Response(null, {
+        headers: {
+          ...APP_BRIDGE_HEADERS,
+          'Access-Control-Max-Age': '7200'
+        }
+      });
     }
   }
 
