acafaf89dfc19836a57e65c3b92efdb5eb9e1bca
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2023-06-23 09:40:06 -0400

    Remove local shopify-app-remix package

--- shopify-app-remix/src/version.ts
+++
@@ -1,3 +0,0 @@
-// TODO Update the releasing docs to mention updating this value
-// https://github.com/orgs/Shopify/projects/6899/views/1?pane=issue&itemId=28367617
-export const SHOPIFY_REMIX_LIBRARY_VERSION = "0.0.0";
