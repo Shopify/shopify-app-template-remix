2762faacede7462d42c9ef6ad392a0ab2a62a675
Shaun Stanworth <shaun.stanworth@shopify.com>
2023-07-18 12:18:27 +0100

    Add GraphQL config for Remix & extensions

---
+++ .graphqlrc.js
@@ -0,0 +1,35 @@
+const fs = require('node:fs');
+
+function getConfig() {
+    const config = {
+        projects: {
+            shopifyAdminApi: {
+                schema: 'https://shopify.dev/admin-graphql-direct-proxy/2023-07',
+                documents: ['./app/**/*.{graphql,js,ts,jsx,tsx}']
+            }
+        }
+    }
+
+    let extensions = []
+    try {
+        extensions = fs.readdirSync('./extensions');
+    } catch {
+        // ignore if no extensions
+    }
+
+    for (const entry of extensions) {
+        const extensionPath = `./extensions/${entry}`;
+        const schema = `${extensionPath}/schema.graphql`;
+        if(!fs.existsSync(schema)) {
+            continue;
+        }
+        config.projects[entry] = {
+            schema,
+            documents: [`${extensionPath}/input.graphql`]
+        }
+    }
+
+    return config;
+}
+
+module.exports = getConfig();
