8241482c5b7caa4ca7eb38f02eab11c046388837
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2023-09-21 10:52:25 -0400

    Update Remix dependencies to v2

--- app/entry.server.jsx
+++ app/entry.server.jsx
@@ -1,6 +1,5 @@
 import { PassThrough } from "stream";
 import { renderToPipeableStream } from "react-dom/server";
-import { Response } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import isbot from "isbot";
 
--- remix.config.js
+++ remix.config.js
@@ -15,14 +15,6 @@ module.exports = {
   ignoredRouteFiles: ["**/.*"],
   appDirectory: "app",
   serverModuleFormat: "cjs",
-  future: {
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-    v2_dev: {
-      port: process.env.HMR_SERVER_PORT || 8002,
-    },
-  },
+  dev: { port: process.env.HMR_SERVER_PORT || 8002 },
+  future: {},
 };
