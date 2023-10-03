d321b5785cc4cb0d2b2c2fb1231e68fb8207d442
Richard Powell <richard.powell@shopify.com>
2023-10-03 16:09:53 -0400

    Attempt to implement hook using useSubmit

---
+++ app/direct-api/useShopifyAdminSubmit.ts
@@ -0,0 +1,40 @@
+// NOTE: This would be provided by shopify-app-remix
+
+import { useFetcher, useSubmit } from "@remix-run/react";
+import { useCallback, useMemo } from "react";
+
+interface GraphQLQueryOptions {
+  variables?: {
+    [key: string]: any;
+  };
+}
+
+export function useShopifyAdminFetcher() {
+  const submit = useSubmit();
+
+  const graphql = useCallback(
+    (query: string, options: GraphQLQueryOptions) => {
+      submit(
+        { query, variables: options.variables || null },
+        {
+          method: "POST",
+          encType: "application/json",
+
+          // PROBLEM: React router thinks this is a relative path
+          // No matter what I do, it parses this incorrectly
+          action: "shopify:admin/api/graphql.json",
+        }
+      );
+    },
+    [submit]
+  );
+
+  return useMemo(
+    () => ({
+      state,
+      data,
+      graphql,
+    }),
+    [data, graphql, state]
+  );
+}
