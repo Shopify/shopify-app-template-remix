35601e4ff1528390bdc013f0a2e1587a4f43d41c
Richard Powell <richard.powell@shopify.com>
2023-10-20 12:31:02 -0400

    Use the V# Webhook admin context

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -6,6 +6,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
   const { topic, shop, session, admin, payload } = await authenticate.webhook(
     request
   );
+
   if (!admin) {
     // The admin context isn't returned if the webhook fired after a shop was uninstalled.
     throw new Response();
