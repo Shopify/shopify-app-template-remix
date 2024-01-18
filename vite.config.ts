d4c452648953f97cd9aa44e584bc84efa8f3da8c
Paul <phanminh65@gmail.com>
2024-01-18 12:10:40 +0700

    chore: remove installGlobals

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig, type UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-// import { installGlobals } from "@remix-run/node";
-//
-// installGlobals();
+
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
 // stop passing in HOST, so we can remove this workaround after the next major release.
