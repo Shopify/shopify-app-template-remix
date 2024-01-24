4f4ae33d641e6c9adf7b85bc3afd055b29f3a81e
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2024-01-24 15:32:58 -0500

    Merge pull request #484 from Shopify/cd/bump-app-bridge-types-0.0.4
    chore: bump @shopify/app-bridge-types to 0.0.4 for Modal types

---
+++ app/routes/app.tsx
@@ -0,0 +1,40 @@
+import type { HeadersFunction, LoaderFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, Outlet, useLoaderData, useRouteError } from "@remix-run/react";
+import polarisStyles from "@shopify/polaris/build/esm/styles.css";
+import { boundary } from "@shopify/shopify-app-remix/server";
+import { AppProvider } from "@shopify/shopify-app-remix/react";
+import { authenticate } from "../shopify.server";
+
+export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  await authenticate.admin(request);
+
+  return json({ apiKey: process.env.SHOPIFY_API_KEY || "" });
+};
+
+export default function App() {
+  const { apiKey } = useLoaderData<typeof loader>();
+
+  return (
+    <AppProvider isEmbeddedApp apiKey={apiKey}>
+      <ui-nav-menu>
+        <Link to="/app" rel="home">
+          Home
+        </Link>
+        <Link to="/app/additional">Additional page</Link>
+      </ui-nav-menu>
+      <Outlet />
+    </AppProvider>
+  );
+}
+
+// Shopify needs Remix to catch some thrown responses, so that their headers are included in the response.
+export function ErrorBoundary() {
+  return boundary.error(useRouteError());
+}
+
+export const headers: HeadersFunction = (headersArgs) => {
+  return boundary.headers(headersArgs);
+};
