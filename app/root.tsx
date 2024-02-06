b32c99ac38df9255d286c1ec6c5ac8b8d4c7e37a
Aaron Casanova <32409546+aaronccasanova@users.noreply.github.com>
2024-02-06 10:19:54 -0800

    Add Inter font (#539)

---
+++ app/root.tsx
@@ -0,0 +1,32 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="preconnect" href="https://cdn.shopify.com/" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
+        />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <LiveReload />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/_index/route.tsx
@@ -0,0 +1,54 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import { login } from "../../shopify.server";
+import indexStyles from "./style.css";
+
+export const links = () => [{ rel: "stylesheet", href: indexStyles }];
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+
+  if (url.searchParams.get("shop")) {
+    throw redirect(`/app?${url.searchParams.toString()}`);
+  }
+
+  return json({ showForm: Boolean(login) });
+};
+
+export default function App() {
+  const { showForm } = useLoaderData<typeof loader>();
+
+  return (
+    <div className="index">
+      <div className="content">
+        <h1>A short heading about [your app]</h1>
+        <p>A tagline about [your app] that describes your value proposition.</p>
+        {showForm && (
+          <Form method="post" action="/auth/login">
+            <label>
+              <span>Shop domain</span>
+              <input type="text" name="shop" />
+              <span>e.g: my-shop-domain.myshopify.com</span>
+            </label>
+            <button type="submit">Log in</button>
+          </Form>
+        )}
+        <ul>
+          <li>
+            <strong>Product feature</strong>. Some detail about your feature and
+            its benefit to your customer.
+          </li>
+          <li>
+            <strong>Product feature</strong>. Some detail about your feature and
+            its benefit to your customer.
+          </li>
+          <li>
+            <strong>Product feature</strong>. Some detail about your feature and
+            its benefit to your customer.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
