5c29d4b9f9e86713893679c6b948fdbbae589132
Elizabeth Kenyon <elizabeth.kenyon@shopify.com>
2024-04-05 10:05:42 -0500

    Fix API version to April 24
    The API version should be set to a specific version so breaking changes wont accidentatlly be brought in

--- .graphqlrc.ts
+++ .graphqlrc.ts
@@ -1,15 +1,15 @@
 import fs from "fs";
 
-import { LATEST_API_VERSION } from "@shopify/shopify-api";
 import { shopifyApiProject, ApiType } from "@shopify/api-codegen-preset";
 import type { IGraphQLConfig } from "graphql-config";
+import { apiVersion } from "./app/shopify.server";
 
 function getConfig() {
   const config: IGraphQLConfig = {
     projects: {
       default: shopifyApiProject({
         apiType: ApiType.Admin,
-        apiVersion: LATEST_API_VERSION,
+        apiVersion: apiVersion,
         documents: ["./app/**/*.{js,ts,jsx,tsx}"],
         outputDir: "./app/types",
       }),
