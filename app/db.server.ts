f43b02f7646b04ee4d2a25d90b45c0cbe752aaf3
Isaac Roldán <isaac.roldan@shopify.com>
2024-08-21 17:28:09 +0200

    Merge pull request #835 from Shopify/isaacroldan-patch-1
    Update .gitignore

---
+++ app/db.server.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient;
+}
+
+const prisma: PrismaClient = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+}
+
+export default prisma;
---
+++ app/routes/webhooks.tsx
@@ -0,0 +1,32 @@
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { authenticate } from "../shopify.server";
+import db from "../db.server";
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const { topic, shop, session, admin } = await authenticate.webhook(request);
+
+  if (!admin && topic !== 'SHOP_REDACT') {
+    // The admin context isn't returned if the webhook fired after a shop was uninstalled.
+    // The SHOP_REDACT webhook will be fired up to 48 hours after a shop uninstalls the app.
+    // Because of this, no admin context is available.
+    throw new Response();
+  }
+
+  // The topics handled here should be declared in the shopify.app.toml.
+  // More info: https://shopify.dev/docs/apps/build/cli-for-apps/app-configuration
+  switch (topic) {
+    case "APP_UNINSTALLED":
+      if (session) {
+        await db.session.deleteMany({ where: { shop } });
+      }
+
+      break;
+    case "CUSTOMERS_DATA_REQUEST":
+    case "CUSTOMERS_REDACT":
+    case "SHOP_REDACT":
+    default:
+      throw new Response("Unhandled webhook topic", { status: 404 });
+  }
+
+  throw new Response();
+};
