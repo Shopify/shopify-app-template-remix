aea41d73997d6cc3f29b068d769b8399b5ab413d
Surma <surma@surma.dev>
2024-09-16 16:26:05 +0100

    Normalize webhook topics

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -4,18 +4,25 @@ import db from "../db.server";
 
 const HANDLERS = import.meta.glob("./webhooks/*.tsx", { eager: true });
 
+// Turns `CUSTOMERS_DATA_REQUEST` into `customers/data_request`
+function normalizeTopic(topic: string) {
+  // This very much intentionally only replaces the first underscore
+  return topic.toLowerCase().replace("_", "/");
+}
+
 export const action = async ({ request }: ActionFunctionArgs) => {
   const { topic } = await request.json();
+  const normalizedTopic = normalizeTopic(topic);
 
   for(let [path, {TOPIC, action}] of Object.entries(HANDLERS)) {
-    // If the topic is not set, infer it from the file name
+    // If the topic is not explicitly set, infer it from the file name
     if(!TOPIC) {
       const fileName = path.split("/").at(-1);
       if(!fileName) throw new Error(`Invalid webhook file name: ${path}`);
-      TOPIC = fileName.split(".").slice(0, 1).join("/");
+      TOPIC = fileName.replace(".", "/");
     }
 
-    if(topic === TOPIC) {
+    if(normalizedTopic === TOPIC) {
       return action(request);
     }
   }
