78e3f4dd8a0cda3ecfd065fdb96bc34777efe679
Surma <surma@surma.dev>
2024-09-16 16:21:18 +0100

    Move webhooks to a folder and don't use Remix routing

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -2,6 +2,22 @@ import type { ActionFunctionArgs } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
 import db from "../db.server";
 
-export const action = async () => {
-  throw new Response("Unhandled webhook topic", { status: 404 });
+const HANDLERS = import.meta.glob("./webhooks/*.tsx", { eager: true });
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const { topic } = await request.json();
+
+  for(let [path, {TOPIC, action}] of Object.entries(HANDLERS)) {
+    // If the topic is not set, infer it from the file name
+    if(!TOPIC) {
+      const fileName = path.split("/").at(-1);
+      if(!fileName) throw new Error(`Invalid webhook file name: ${path}`);
+      TOPIC = fileName.split(".").slice(0, 1).join("/");
+    }
+
+    if(topic === TOPIC) {
+      return action(request);
+    }
+  }
+  throw new Response(`Unhandled webhook topic: ${topic}`, { status: 404 });
 };
--- app/routes/webhooks/app.uninstalled.tsx
+++ app/routes/webhooks/app.uninstalled.tsx
@@ -1,6 +1,9 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
-import { authenticate } from "../shopify.server";
-import db from "../db.server";
+import { authenticate } from "../../shopify.server";
+import db from "../../db.server";
+
+// Optional. If not provided, the topic will be inferred from the file name.
+// export const TOPIC = "app/uninstalled";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const { shop, session, admin } = await authenticate.webhook(request);
