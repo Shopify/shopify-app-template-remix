65ae130f0974e3832ce5b9d42861c94e29af9d1f
Richard Powell <richard.powell@shopify.com>
2024-09-16 10:30:42 -0400

    Remove shop check for customer webhooks

--- app/routes/webhooks.customers.redact.tsx
+++ app/routes/webhooks.customers.redact.tsx
@@ -2,17 +2,12 @@ import type { ActionFunctionArgs } from "@remix-run/node";
 import { authenticate } from "~/shopify.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-  const {session} = await authenticate.webhook(request);
-
-  // Webhook requests can trigger after an app is uninstalled
-  // If the app is already uninstalled, the session may be undefined.
-  if (!session) {
-    throw new Response();
-  }
+  const { shop, payload} = await authenticate.webhook(request);
 
   // Implement handling of mandatory compliance topics
   // See: https://shopify.dev/docs/apps/build/privacy-law-compliance
-  console.log("Received customers redact webhook");
+  console.log(`Received customers redact for ${shop}`);
+  console.log(JSON.stringify(payload, null, 2));
 
   return new Response();
 };
