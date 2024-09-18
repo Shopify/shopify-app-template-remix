5cd43d6e4c4d3e9d42a961c9b3dafd1e35ef4295
GitHub <noreply@github.com>
2024-09-18 14:21:49 +0000

    Convert template to Javascript

--- app/routes/webhooks.customers.redact.jsx
+++ app/routes/webhooks.customers.redact.jsx
@@ -1,4 +1,4 @@
-import { authenticate } from "~/shopify.server";
+import { authenticate } from "../shopify.server";
 
 export const action = async ({ request }) => {
   const { shop, payload } = await authenticate.webhook(request);
