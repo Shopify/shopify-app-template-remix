04624ea49b0d3bc1e23f3327b6d22304addb9742
Richard Powell <richard.powell@shopify.com>
2024-09-19 09:12:22 -0400

    use the topic in every console log for webhook handlers

--- app/routes/webhooks.app.uninstalled.tsx
+++ app/routes/webhooks.app.uninstalled.tsx
@@ -3,9 +3,9 @@ import { authenticate } from "../shopify.server";
 import db from "../db.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-  const { shop, session } = await authenticate.webhook(request);
+  const { shop, session, topic } = await authenticate.webhook(request);
 
-  console.log(`Received app uninstalled for ${shop}`);
+  console.log(`Received ${topic} webhook for ${shop}`);
 
   // Webhook requests can trigger multiple times and after an app has already been uninstalled.
   // If this webhook already ran, the session may have been deleted previously.
