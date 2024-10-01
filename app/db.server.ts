32d7bd528875d283a9d80bfb20a8c958873b7332
Surma <surma@surma.dev>
2024-10-01 12:30:26 +0100

    Fix race-y bug in db.server.ts

--- app/db.server.ts
+++ app/db.server.ts
@@ -4,12 +4,12 @@ declare global {
   var prisma: PrismaClient;
 }
 
-const prisma: PrismaClient = global.prisma || new PrismaClient();
-
 if (process.env.NODE_ENV !== "production") {
   if (!global.prisma) {
     global.prisma = new PrismaClient();
   }
 }
+
+const prisma: PrismaClient = global.prisma || new PrismaClient();
 
 export default prisma;
