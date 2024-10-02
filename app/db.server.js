830a618926341b740dcfd553f2660b7ec8c7eeb7
GitHub <noreply@github.com>
2024-10-02 14:53:41 +0000

    Convert template to Javascript

--- app/db.server.js
+++ app/db.server.js
@@ -1,11 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = global.prisma || new PrismaClient();
-
 if (process.env.NODE_ENV !== "production") {
   if (!global.prisma) {
     global.prisma = new PrismaClient();
   }
 }
+
+const prisma = global.prisma || new PrismaClient();
 
 export default prisma;
