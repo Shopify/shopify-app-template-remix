ebc524361e69745a6d9dcb3cdb6f6532ccd47fbc
Andy Liu <59021924+andy-liuu@users.noreply.github.com>
2024-10-02 16:34:27 -0400

    Update API version and shopify-app-remix version (#863)
    * api version and remix package version
    
    * changelog
    
    * update pr link
    
    * Update CHANGELOG.md

---
+++ app/routes/webhooks.customers.data_request.tsx
@@ -0,0 +1,13 @@
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { authenticate } from "../shopify.server";
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const { shop, payload, topic } = await authenticate.webhook(request);
+
+  // Implement handling of mandatory compliance topics
+  // See: https://shopify.dev/docs/apps/build/privacy-law-compliance
+  console.log(`Received ${topic} webhook for ${shop}`);
+  console.log(JSON.stringify(payload, null, 2));
+
+  return new Response();
+};
