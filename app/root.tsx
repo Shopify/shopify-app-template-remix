f25ebf11ce29b2dd5b8fec54c2ad09ca6044d214
Richard Powell <richard.powell@shopify.com>
2024-10-21 13:57:11 -0400

    Remove /app/ from the apps URL since we no longer have the marketing index page

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,29 @@
+import { type HeadersFunction, json, type LoaderFunction } from "@remix-run/node";
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useRouteError,
 } from "@remix-run/react";
+import { AppProvider } from "@shopify/shopify-app-remix/react";
+import { boundary } from "@shopify/shopify-app-remix/server";
+import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
+import { authenticate } from "./shopify.server";
+
+export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
+
+export const loader: LoaderFunction = async ({ request }) => {
+  await authenticate.admin(request);
+
+  return json({ apiKey: process.env.SHOPIFY_API_KEY || "" });
+};
 
 export default function App() {
+  const { apiKey } = useLoaderData<typeof loader>();
+
   return (
     <html>
       <head>
@@ -21,10 +38,21 @@ export default function App() {
         <Links />
       </head>
       <body>
+      <AppProvider isEmbeddedApp apiKey={apiKey}>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
+      </AppProvider>
+      <ScrollRestoration />
+      <Scripts />
       </body>
     </html>
   );
 }
+
+// Shopify needs Remix to catch some thrown responses, so that their headers are included in the response.
+export function ErrorBoundary() {
+  return boundary.error(useRouteError());
+}
+
+export const headers: HeadersFunction = (headersArgs) => {
+  return boundary.headers(headersArgs);
+};
