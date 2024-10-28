5b71c859447d2646ba9f90fc863cc03a8e33a8aa
Allen Chazhoor <allen.chazhoor@shopify.com>
2024-10-28 14:15:06 -0400

    add webhook logs

--- app/routes/webhooks.app.scopes_update.tsx
+++ app/routes/webhooks.app.scopes_update.tsx
@@ -3,7 +3,9 @@ import { authenticate } from "../shopify.server";
 import db from "../db.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-    const { payload, session } = await authenticate.webhook(request);
+    const { payload, session, topic, shop } = await authenticate.webhook(request);
+    console.log(`Received ${topic} webhook for ${shop}`);
+
     const current = payload.current as string[];
     if (session) {
         await db.session.update({   
