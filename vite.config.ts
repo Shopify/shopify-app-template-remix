834392a6aa73151cefb3fb2a6070741c678cd212
Elizabeth Kenyon <elizabeth.kenyon@shopify.com>
2024-11-25 14:40:24 -0600

    Enable v3_throwAbortReason

--- vite.config.ts
+++ vite.config.ts
@@ -49,6 +49,7 @@ export default defineConfig({
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
       },
     }),
     tsconfigPaths(),
