696f7ca96de225b91c4df8ae81edfeb51cc6f95a
Hướng <vbh30062001@gmail.com>
2024-12-18 17:52:07 +0700

    remove prisma

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,15 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import { JSONFilePreset } from 'lowdb/node';
 
-declare global {
-  var prisma: PrismaClient;
+const createDatabase = async () => {
+  const db = await JSONFilePreset('db.json', { entities: []})
+  return db;
 }
 
-if (process.env.NODE_ENV !== "production") {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-}
-
-const prisma: PrismaClient = global.prisma || new PrismaClient();
-
-export default prisma;
+export const db = createDatabase();
--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -4,8 +4,8 @@ import {
   AppDistribution,
   shopifyApp,
 } from "@shopify/shopify-app-remix/server";
-import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
-import prisma from "./db.server";
+import { MemorySessionStorage } from '@shopify/shopify-app-session-storage-memory';
+
 
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
@@ -14,7 +14,7 @@ const shopify = shopifyApp({
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
-  sessionStorage: new PrismaSessionStorage(prisma),
+  sessionStorage: new MemorySessionStorage(),
   distribution: AppDistribution.AppStore,
   future: {
     unstable_newEmbeddedAuthStrategy: true,
