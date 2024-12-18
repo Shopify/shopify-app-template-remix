006c9d719fce9c5469f04518b974556823c5dc99
Hướng <vbh30062001@gmail.com>
2024-12-18 18:11:51 +0700

    hidden uninstall

--- app/routes/webhooks.app.uninstalled.tsx
+++ app/routes/webhooks.app.uninstalled.tsx
@@ -1,6 +1,6 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
-import db from "../db.server";
+// import { db } from "../db.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const { shop, session, topic } = await authenticate.webhook(request);
@@ -10,7 +10,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
   // Webhook requests can trigger multiple times and after an app has already been uninstalled.
   // If this webhook already ran, the session may have been deleted previously.
   if (session) {
-    await db.session.deleteMany({ where: { shop } });
+    // await db.session.deleteMany({ where: { shop } });
   }
 
   return new Response();
