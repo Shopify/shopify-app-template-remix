8bd03f192a762b04b234a876af63fe08b840dd01
GitHub <noreply@github.com>
2025-01-31 19:37:40 +0000

    Convert template to Javascript

--- app/routes/auth.$.tsx
+++
@@ -1,8 +0,0 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
-import { authenticate } from "../shopify.server";
-
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-  await authenticate.admin(request);
-
-  return null;
-};
