58f1bb48e7f2fbff2656bdcfbc02a766aeb2315d
Richard Powell <richard.powell@shopify.com>
2025-02-03 16:14:13 -0500

    Add app-bridge-react and polaris to optimizeDeps
    In development its possible (but an edge case) that navigating from one page without these dependencies to another page with the dependencies would cause a full page refresh, which then redirects to the logicn page.  By adding this config, we should avoid that edge case

--- vite.config.ts
+++ vite.config.ts
@@ -63,4 +63,7 @@ export default defineConfig({
   build: {
     assetsInlineLimit: 0,
   },
+  optimizeDeps: {
+    include: ["@shopify/app-bridge-react", "@shopify/polaris"],
+  },
 }) satisfies UserConfig;
