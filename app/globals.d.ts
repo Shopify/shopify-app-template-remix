69ba48cb816769c47bd248e61704dec75720a7bf
Kyle Durand <kyle.durand@shopify.com>
2025-05-14 17:17:51 -0400

    add @shopify/app-bridge-ui-types

--- app/globals.d.ts
+++ app/globals.d.ts
@@ -1,10 +1,1 @@
 declare module "*.css";
-
-declare namespace JSX {
-  interface IntrinsicElements {
-    [elemName: `s-${string}` | `ui-${string}`]: {
-      [key: string]: any;
-      children?: React.ReactNode;
-    };
-  }
-}
