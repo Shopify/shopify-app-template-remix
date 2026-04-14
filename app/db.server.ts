62ec4af93226c40f4d66b149b534f326b00b6d25
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2026-04-14 16:18:19 +0200

    Merge pull request #1230 from Shopify/remove-npx
    Avoid npx in shopify.web.toml

---
+++ app/db.server.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prismaGlobal: PrismaClient;
+}
+
+if (process.env.NODE_ENV !== "production") {
+  if (!global.prismaGlobal) {
+    global.prismaGlobal = new PrismaClient();
+  }
+}
+
+const prisma = global.prismaGlobal ?? new PrismaClient();
+
+export default prisma;
